ebaa3b41dd5c446e7d7b3b5863c30709d0521b74
Freek Bes <freek.bes@codam.nl>
2024-10-16 15:49:35 +0200

    show current coalition next to the quiz button

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -2,11 +2,14 @@ import { Express } from 'express';
 import passport from 'passport';
 import { PrismaClient } from '@prisma/client';
 import { isQuizAvailable } from './quiz';
+import { ExpressIntraUser } from '../sync/oauth';
 
 export const setupHomeRoutes = function(app: Express, prisma: PrismaClient): void {
 	app.get('/', passport.authenticate('session', {
 		keepSessionInfo: true,
 	}), async (req, res) => {
+		const user = req.user as ExpressIntraUser;
+
 		// Get all coalitions
 		const coalitions = await prisma.codamCoalition.findMany({
 			select: {
@@ -24,11 +27,35 @@ export const setupHomeRoutes = function(app: Express, prisma: PrismaClient): voi
 			}
 		});
 
+		// Get the coalition of the current user
+		const my_coalition = await prisma.intraCoalitionUser.findFirst({
+			where: {
+				user_id: user.id,
+			},
+			select: {
+				coalition: {
+					select: {
+						id: true,
+						name: true,
+						color: true,
+						image_url: true,
+						codam_coalition: {
+							select: {
+								description: true,
+								tagline: true,
+							},
+						},
+					},
+				},
+			},
+		});
+
 		// Check if quiz is currently available
 		const quiz_available = await isQuizAvailable(prisma);
 
 		return res.render('home.njk', {
 			coalitions,
+			my_coalition,
 			quiz_available,
 		});
 	});
