f57391f3b2b428321a1917cc48924f4486ad166b
Freek Bes <freek.bes@codam.nl>
2024-12-06 14:23:35 +0100

    feat: take bloc deadlines into account for score calcualtions

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { Express } from 'express';
 import { ExpressIntraUser } from '../sync/oauth';
-import { getUserRankingAcrossAllRankings, getUserTournamentRanking } from '../utils';
+import { getBlocAtDate, getUserRankingAcrossAllRankings, getUserTournamentRanking } from '../utils';
 
 export const setupProfileRoutes = function(app: Express, prisma: PrismaClient): void {
 	app.get('/profile/:login', async (req, res) => {
@@ -28,20 +28,24 @@ export const setupProfileRoutes = function(app: Express, prisma: PrismaClient): 
 			return res.status(404).send('User not found');
 		}
 
-		// TODO: take tournament deadlines into account
-		const totalScores = await prisma.codamCoalitionScore.groupBy({
+		const now = new Date();
+		const currentBloc = await getBlocAtDate(prisma, now);
+		const totalScores = (currentBloc ? (await prisma.codamCoalitionScore.groupBy({
 			by: ['fixed_type_id'],
 			_sum: {
 				amount: true,
 			},
 			where: {
 				user_id: profileUser.id,
+				created_at: {
+					gte: currentBloc.begin_at,
+					lt: now,
+				},
 			},
-		});
+		})) : []);
 		const totalScore = totalScores.reduce((acc, score) => acc + (score._sum.amount ? score._sum.amount : 0), 0);
 		const ranking = await getUserTournamentRanking(prisma, profileUser.id);
 
-		// TODO: take tournament deadlines into account
 		const latestScores = await prisma.codamCoalitionScore.findMany({
 			where: {
 				user_id: profileUser.id,
