81fa5c5de8a1d154d4ee0f54aa472bfc8d67eb29
Freek Bes <freek.bes@codam.nl>
2026-01-13 18:00:29 +0100

    fix: make it possible to view coalition pages without an ongoing season

--- src/routes/coalitions.ts
+++ src/routes/coalitions.ts
@@ -28,112 +28,6 @@ export const setupCoalitionRoutes = function(app: Express, prisma: PrismaClient)
 			return res.status(404).send('Coalition not found');
 		}
 
-		const bloc = await getBlocAtDate(prisma, now);
-		if (!bloc) {
-			// No ongoing season, there's no point in viewing this page. Redirect to home.
-			return res.redirect('/');
-		}
-
-		// Get current coalition score
-		const coalitionScore = await getCoalitionScore(prisma, coalition.id);
-
-		// Get the top 25 contributors of this season
-		const topContributors = await getCoalitionTopContributors(prisma, coalition.id, 'Top contributors of this season', now, 25);
-
-		// Get the top contributors of the past 7 days
-		const sevenDaysAgo = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
-		if (sevenDaysAgo < bloc.begin_at) {
-			// If the season started less than 7 days ago, we can't get the top contributors of the past 7 days.
-			// Get the top contributors since the season's start instead.
-			sevenDaysAgo.setTime(bloc.begin_at.getTime());
-			console.log('Season started less than 7 days ago, getting top contributors since season start instead');
-		}
-		const topScoresWeek = await prisma.codamCoalitionScore.groupBy({
-			by: ['user_id'],
-			_sum: {
-				amount: true,
-			},
-			orderBy: {
-				_sum: {
-					amount: 'desc',
-				},
-			},
-			where: {
-				coalition_id: coalition.id,
-				created_at: {
-					gte: sevenDaysAgo,
-					lte: now,
-				},
-			},
-			take: 10,
-		});
-		const topContributorsWeek = await scoreSumsToRanking(prisma, topScoresWeek, 'Top contributors of the past 7 days');
-
-		const latestScores = await prisma.codamCoalitionScore.findMany({
-			where: {
-				coalition_id: coalition.id,
-			},
-			orderBy: {
-				created_at: 'desc',
-			},
-			include: {
-				user: {
-					select: {
-						intra_user: {
-							select: {
-								login: true,
-								usual_full_name: true,
-								image: true,
-							},
-						},
-					},
-				},
-			},
-			take: 50,
-		});
-
-		const latestBigScores = await prisma.codamCoalitionScore.findMany({
-			where: {
-				coalition_id: coalition.id,
-				OR: [
-					{
-						NOT: {
-							fixed_type_id: {
-								in: SMALL_CONTRIBUTION_TYPES, // Exclude usually low individual scores
-							}
-						},
-					},
-					{
-						fixed_type_id: null, // Do include scores that are not fixed types
-					}
-				],
-				amount: {
-					gt: 0,
-				},
-				created_at: {
-					gte: sevenDaysAgo,
-					lte: now,
-				},
-			},
-			orderBy: {
-				created_at: 'desc',
-			},
-			include: {
-				user: {
-					select: {
-						intra_user: {
-							select: {
-								login: true,
-								usual_full_name: true,
-								image: true,
-							},
-						},
-					},
-				},
-			},
-			take: 25,
-		});
-
 		// Get the staff part of this coalition
 		const staff = await prisma.intraUser.findMany({
 			where: {
@@ -176,17 +70,124 @@ export const setupCoalitionRoutes = function(app: Express, prisma: PrismaClient)
 			});
 		}
 
-		return res.render('coalition.njk', {
+		const currentBloc = await getBlocAtDate(prisma, now);
+		const viewOptions: any = {
 			coalition,
-			topContributors,
-			topContributorsWeek,
-			latestScores,
-			latestBigScores,
-			coalitionScore,
+			currentBloc,
 			staff,
 			assistantGroup,
 			assistantsCanQuiz: ASSISTANTS_CAN_QUIZ,
 			assistants,
-		});
+		};
+
+		if (currentBloc) {
+			// Get current coalition score
+			const coalitionScore = await getCoalitionScore(prisma, coalition.id);
+
+			// Get the top 25 contributors of this season
+			const topContributors = await getCoalitionTopContributors(prisma, coalition.id, 'Top contributors of this season', now, 25);
+
+			// Get the top contributors of the past 7 days
+			const sevenDaysAgo = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
+			if (sevenDaysAgo < currentBloc.begin_at) {
+				// If the season started less than 7 days ago, we can't get the top contributors of the past 7 days.
+				// Get the top contributors since the season's start instead.
+				sevenDaysAgo.setTime(currentBloc.begin_at.getTime());
+				console.log('Season started less than 7 days ago, getting top contributors since season start instead');
+			}
+			const topScoresWeek = await prisma.codamCoalitionScore.groupBy({
+				by: ['user_id'],
+				_sum: {
+					amount: true,
+				},
+				orderBy: {
+					_sum: {
+						amount: 'desc',
+					},
+				},
+				where: {
+					coalition_id: coalition.id,
+					created_at: {
+						gte: sevenDaysAgo,
+						lte: now,
+					},
+				},
+				take: 10,
+			});
+			const topContributorsWeek = await scoreSumsToRanking(prisma, topScoresWeek, 'Top contributors of the past 7 days');
+
+			const latestScores = await prisma.codamCoalitionScore.findMany({
+				where: {
+					coalition_id: coalition.id,
+				},
+				orderBy: {
+					created_at: 'desc',
+				},
+				include: {
+					user: {
+						select: {
+							intra_user: {
+								select: {
+									login: true,
+									usual_full_name: true,
+									image: true,
+								},
+							},
+						},
+					},
+				},
+				take: 50,
+			});
+
+			const latestBigScores = await prisma.codamCoalitionScore.findMany({
+				where: {
+					coalition_id: coalition.id,
+					OR: [
+						{
+							NOT: {
+								fixed_type_id: {
+									in: SMALL_CONTRIBUTION_TYPES, // Exclude usually low individual scores
+								}
+							},
+						},
+						{
+							fixed_type_id: null, // Do include scores that are not fixed types
+						}
+					],
+					amount: {
+						gt: 0,
+					},
+					created_at: {
+						gte: sevenDaysAgo,
+						lte: now,
+					},
+				},
+				orderBy: {
+					created_at: 'desc',
+				},
+				include: {
+					user: {
+						select: {
+							intra_user: {
+								select: {
+									login: true,
+									usual_full_name: true,
+									image: true,
+								},
+							},
+						},
+					},
+				},
+				take: 25,
+			});
+
+			viewOptions['topContributors'] = topContributors;
+			viewOptions['topContributorsWeek'] = topContributorsWeek;
+			viewOptions['latestScores'] = latestScores;
+			viewOptions['latestBigScores'] = latestBigScores;
+			viewOptions['coalitionScore'] = coalitionScore;
+		}
+
+		return res.render('coalition.njk', viewOptions);
 	});
 };
