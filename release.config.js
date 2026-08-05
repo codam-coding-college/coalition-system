0eb720fc36c63158f979d6a49e3ad4b19ad9d7dd
Freek Bes <freek.bes@codam.nl>
2026-08-05 14:20:26 +0200

    fix GitHub workflow container image tagging

--- release.config.js
+++ release.config.js
@@ -6,7 +6,7 @@ module.exports = {
 		[
 			"@semantic-release/exec",
 			{
-				publishCmd: 'docker tag ghcr.io/codam-coding-college/peerpp-bot ghcr.io/codam-coding-college/peerpp-bot:${nextRelease.version} && docker push ghcr.io/codam-coding-college/peerpp-bot:${nextRelease.version}'
+				publishCmd: 'docker tag ghcr.io/codam-coding-college/coalition-system ghcr.io/codam-coding-college/coalition-system:${nextRelease.version} && docker push ghcr.io/codam-coding-college/coalition-system:${nextRelease.version}'
 			}
 		],
 		[
